5bfff2ae951075cd82ba8ed931fb90faf6863026
moonjay1013 <1318027823@qq.com>
2024-06-18 00:44:41 +0800

    springboot init

---
+++ src/main/java/com/moon/HahaVideoApplication.java
@@ -0,0 +1,13 @@
+package com.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahaVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HahaVideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/HahaVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahaVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
