2ac0bfcb63eb31207b2ab47add52b8476b0ae185
moonjay1013 <1318027823@qq.com>
2024-06-18 22:22:10 +0800

    Integrated JPA & mysql-connector & DB: t_users

--- src/main/java/com/moon/pojo/User.java
+++ src/main/java/com/moon/pojo/User.java
@@ -3,93 +3,69 @@ package com.moon.pojo;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.*;
+
 /**
  * @author moonjay
  */
 @ApiModel(value = "用户实体类", description = "用户信息描述")
+@Entity
+@Table(name = "t_users")
 public class User {
     /** 主键设置为id id自增长*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 64)
     @ApiModelProperty(hidden = true,name = "id",value = "用户id（唯一）")
-
     private Integer id;
-
+    @Column(name = "username", length = 20, nullable = false)
     private String name;
-
+    @Column(name = "password", length = 64, nullable = false)
     private String pwd;
-
+    @Column(name = "face_image")
     private String faceImage;
-
+    @Column(length = 20, nullable = false)
     private String nickname;
-
+    @Column(name = "fans_counts", length = 11)
     private Integer fansCounts;
-
+    @Column(name = "follow_counts", length = 11)
     private Integer followCounts;
-
+    @Column(name = "receive_like_counts", length = 11)
     private Integer receiveLikes;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    /** Setter & Getter */
+    public Integer getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
 
-    public String getPwd() {
-        return pwd;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
+    public String getPwd() { return pwd; }
 
-    public String getFaceImage() {
-        return faceImage;
-    }
+    public void setPwd(String pwd) { this.pwd = pwd; }
 
-    public void setFaceImage(String faceImage) {
-        this.faceImage = faceImage;
-    }
+    public String getFaceImage() { return faceImage; }
 
-    public String getNickname() {
-        return nickname;
-    }
+    public void setFaceImage(String faceImage) { this.faceImage = faceImage; }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+    public String getNickname() { return nickname; }
 
-    public Integer getFansCounts() {
-        return fansCounts;
-    }
+    public void setNickname(String nickname) { this.nickname = nickname; }
 
-    public void setFansCounts(Integer fansCounts) {
-        this.fansCounts = fansCounts;
-    }
+    public Integer getFansCounts() { return fansCounts; }
 
-    public Integer getFollowCounts() {
-        return followCounts;
-    }
+    public void setFansCounts(Integer fansCounts) { this.fansCounts = fansCounts; }
 
-    public void setFollowCounts(Integer followCounts) {
-        this.followCounts = followCounts;
-    }
+    public Integer getFollowCounts() { return followCounts; }
 
-    public Integer getReceiveLikes() {
-        return receiveLikes;
-    }
+    public void setFollowCounts(Integer followCounts) { this.followCounts = followCounts; }
 
-    public void setReceiveLikes(Integer receiveLikes) {
-        this.receiveLikes = receiveLikes;
-    }
+    public Integer getReceiveLikes() { return receiveLikes; }
+
+    public void setReceiveLikes(Integer receiveLikes) { this.receiveLikes = receiveLikes; }
 
     @Override
     public String toString() {
