86ec91699091ddd2bbbde7320c1a26490c8f8fe9
moonjay1013 <1318027823@qq.com>
2024-06-19 12:52:38 +0800

    SpringBoot Test, junit test

--- src/test/java/com/moon/HahaVideoApplicationTests.java
+++ src/test/java/com/moon/HahaVideoApplicationTests.java
@@ -1,13 +1,51 @@
 package com.moon;
 
+import com.moon.pojo.User;
+import com.moon.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HahaVideoApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     void contextLoads() {
+        System.out.println("This is Unit Test class");
+    }
+
+    @Test
+    void testUserInsert(){
+        /* org.junit.jupiter.api.extension.ParameterResolutionException:
+          No ParameterResolver registered for parameter
+          @Test func no need parameters
+         */
+        User user = new User();
+        user.setFaceImage("");
+        user.setName("moonjay");
+        user.setNickname("moonjay");
+        user.setPwd("123456");
+
+        try {
+            userService.insertUser(user);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("user insert func completed!");
+        }
+    }
+
+    @Test
+    void testFindByNP(){
+        try {
+            User u = userService.findUser("moonjay", "123456");
+            System.out.println(u.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
